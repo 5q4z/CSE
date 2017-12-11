fb25a0d5e3ac8919822260729b8bf9f0115bb33e
5q4z <5q4z@github.com>
2017-12-11 10:43:58 -0800

    12/11/17

--- Logan Yang- Lucky7's.py
+++ Logan Yang- Lucky7's.py
@@ -1,9 +1,16 @@
 import random
 # Logan Yang
 
-
-b = 15
-c = (random.randint(1, 6))
-d = (random.randint(1, 6))
-if c + d == 7:
-    
+money = 15
+rounds = 0
+d1 = (random.randint(1, 6))
+d2 = (random.randint(1, 6)) 
+if int(money) >= 1:
+    int(rounds) + int(1)
+    if d1 + d2 == 7:
+        int(money) - int(1)
+        int(money) + int(5)
+    elif d1 + d2 != 7:
+        int(money) - int(1)
+    print(d1 + d2)
+print(rounds)
